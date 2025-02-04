235e58b799fb0b9569bc3c3d86fe3d73ee55a684
thcrt <110127860+thcrt@users.noreply.github.com>
2025-02-04 01:26:13 +0100

    Support text posts

--- src/archedbrows/downloader.py
+++ src/archedbrows/downloader.py
@@ -15,12 +15,16 @@ class Downloader:
 
     def download_post(self, url: str) -> Post:
         download_process = subprocess.run(
-            ["gallery-dl", "--write-metadata", "-d", str(self.download_dir), url],
+            ["gallery-dl", "--write-info-json", "-D", str(self.download_dir), url],
             capture_output=True,
             text=True,
         )
-        media_path = Path(download_process.stdout.strip("\n# "))
-        info_path = media_path.with_suffix(media_path.suffix + ".json")
+
+        info_path = self.download_dir / "info.json"
+        media_path = None
+        if download_process.stdout.strip("\n# "):
+            media_path = Path(download_process.stdout.strip("\n# "))
+
 
         info = json.loads(info_path.read_text())
 
@@ -30,9 +34,10 @@ class Downloader:
             time=datetime.fromisoformat(info["date"]),
             source=f"{info['category']} {info['subcategory']}",
             source_url=url,
-            text=info["selftext"] if info["selftext"] else None,
+            text=info["selftext_html"],
         )
 
-        post.media.append(Media(media_path.read_bytes()))
+        if media_path:
+            post.media.append(Media(media_path.read_bytes()))
 
         return post
