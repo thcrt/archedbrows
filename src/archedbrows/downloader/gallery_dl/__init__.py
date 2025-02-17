8ec622becce9543fbb8a26dd81f6ce6cef74aacb
thcrt <110127860+thcrt@users.noreply.github.com>
2025-02-17 20:52:07 +0100

    Switch to `gallery-dl-bytes` API

--- src/archedbrows/downloader/gallery_dl/__init__.py
+++ src/archedbrows/downloader/gallery_dl/__init__.py
@@ -1,34 +1,37 @@
 import mimetypes
 from typing import override
 
-import gallery_dl.config
-from gallery_dl.exception import NoExtractorError
+from gallery_dl.exception import NoExtractorError  # type: ignore
+from gallery_dl_bytes import DownloadJob, set_config
 
 from archedbrows.downloader.common import Downloader, UnsupportedURLError
 from archedbrows.models import Media, Post
 
-from ._memory import InMemoryDownloadJob
 from .metadata import parse_post
 
-gallery_dl.config.load()
-gallery_dl.config.set(("extractor",), "metadata", True)
-gallery_dl.config.set(("extractor",), "browser", "firefox")
+set_config(("extractor",), "metadata", True)
+set_config(("extractor",), "browser", "firefox")
 
 
 class GalleryDLDownloader(Downloader):
-    def __init__(self, url: str):
+    def __init__(self, url: str) -> None:
         super().__init__(url)
         try:
-            self.job = InMemoryDownloadJob(url)
+            self.job = DownloadJob(url)
         except NoExtractorError as e:
             raise UnsupportedURLError(url) from e
 
     @override
     def run(self) -> Post:
         self.job.run()
+        files = self.job.files
         post = parse_post(self.url, self.job.metadata)
-        for filename, file in self.job.files.items():
+        for file in files:
             post.media.append(
-                Media(file, filename=filename, mime_type=mimetypes.guess_file_type(filename)[0])
+                Media(
+                    file.data,
+                    filename=str(file.name),
+                    mime_type=mimetypes.guess_file_type(str(file.name))[0],
+                )
             )
         return post
