a5e61c4876ac9bc43096acbd66dbe26502ca7a7e
thcrt <110127860+thcrt@users.noreply.github.com>
2025-02-22 12:18:14 +0100

    Use networking type stubs newly added to `yt-dlp-types` package

--- typings/yt_dlp/networking/_helper.pyi
+++
@@ -1,7 +0,0 @@
-from collections.abc import Callable
-
-from .common import Request, RequestHandler, Response
-
-def wrap_request_errors(
-    func: Callable[[RequestHandler, Request], Response | None],
-) -> Callable[[RequestHandler, Request], None]: ...
