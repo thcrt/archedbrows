50c44edc49156e86faab5a6768550e4122c974c5
thcrt <110127860+thcrt@users.noreply.github.com>
2025-02-23 17:36:46 +0100

    Add React

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,4 +1,9 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  
   server: {
     port: 3000,
     strictPort: true,
@@ -9,4 +14,4 @@ export default {
       '@': '/src',
     }
   }
-};
+})
